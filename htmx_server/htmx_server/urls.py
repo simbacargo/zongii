4a3ec5dffd31712fdffd6b35076865bfada7b025
nsaaro <moi@nsaaro.com>
2026-05-20 14:09:46 +0300

    fixed bug

--- htmx_server/htmx_server/urls.py
+++ htmx_server/htmx_server/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
 ]
 
 from django.conf import settings
-from django.conf.urls.static import static
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^inventory/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
